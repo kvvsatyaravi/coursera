36c1f3dd5bb368ea9735c8f237d178fc7abec071
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2020-08-01 11:26:34 +0530

    introduction redux

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,7 +7,6 @@ import React, { Component } from 'react';
 class Header extends Component {
     constructor(props) {
         super(props);
-    
         this.toggleNav = this.toggleNav.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
         this.handleLogin = this.handleLogin.bind(this);
