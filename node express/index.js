e1ebd805ca1dfa0ba87058dff8e583b526e39418
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2020-08-10 15:32:27 +0530

    express assignment 1

--- node express/index.js
+++ node express/index.js
@@ -3,8 +3,14 @@ const express = require('express'),
 const hostname = 'localhost';
 const port = 3000;
 const app = express();
+
 const dishRouter = require('./routes/dishrouter');
-const dishIdRouter = require('./routes/dishrouter')
+const dishIdRouter = require('./routes/dishrouter');
+const leaderRouter = require('./routes/leaderrouter');
+const leaderIdRouter = require('./routes/leaderrouter');
+const promoRouter = require('./routes/promorouter');
+const promoIdRouter = require('./routes/promorouter');
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const morgan = require('morgan');
@@ -12,6 +18,10 @@ app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));app.use('/dishes', dishRouter);
 app.use('/dishes/:dishId',dishIdRouter);
+app.use(express.static(__dirname + '/public'));app.use('/promotions', promoRouter);
+app.use('/promotions/:promoId',promoIdRouter);
+app.use(express.static(__dirname + '/public'));app.use('/leader', leaderRouter);
+app.use('/leaders/:leadId',leaderIdRouter);
 
 const server = http.createServer(app);
 
