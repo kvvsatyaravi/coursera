5c515ebdff3ad585fc297c308148340fc2b86adf
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2020-08-17 18:06:09 +0530

    express,mongoose,mongodb example 1

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -4,8 +4,17 @@ const hostname = 'localhost';
 const port = 3000;
 const app = express();
 
+const mongoose = require('mongoose');
+const Dishes = require('./models/dishes');
+const url = 'mongodb://localhost:27017/conFusion';
+const connect = mongoose.connect(url);
+connect.then((db) => {
+    console.log("Connected correctly to server");
+}, (err) => { 
+  console.log(err); 
+});
+
 const dishRouter = require('./routes/dishrouter');
-const dishIdRouter = require('./routes/dishrouter');
 const leaderRouter = require('./routes/leaderrouter');
 const leaderIdRouter = require('./routes/leaderrouter');
 const promoRouter = require('./routes/promorouter');
@@ -17,7 +26,7 @@ const morgan = require('morgan');
 app.use(morgan('dev'));
 
 app.use(express.static(__dirname + '/public'));app.use('/dishes', dishRouter);
-app.use('/dishes/:dishId',dishIdRouter);
+app.use('/dishes/:dishId',dishRouter);
 app.use(express.static(__dirname + '/public'));app.use('/promotions', promoRouter);
 app.use('/promotions/:promoId',promoIdRouter);
 app.use(express.static(__dirname + '/public'));app.use('/leader', leaderRouter);
