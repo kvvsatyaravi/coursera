5a9e1499fd15cbabfad73174b02fc60a0e6a5fcf
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2020-08-20 11:52:40 +0530

    cookies

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -20,32 +20,48 @@ const promotionRouter = require('./routes/promorouter');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const morgan = require('morgan');
+const cookieParser =require('cookie-parser');
 app.use(morgan('dev'));
-function auth (req, res, next) {
-  console.log(req.headers);
-  var authHeader = req.headers.authorization;
-  if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      next(err);
-      return;
-  }
 
-  var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var user = auth[0];
-  var pass = auth[1];
-  if (user == 'admin' && pass == 'password') {
-      next(); // authorized
-  } else {
-      var err = new Error('You are not authenticated!');
-      res.setHeader('WWW-Authenticate', 'Basic');      
-      err.status = 401;
-      next(err);
+app.use(cookieParser('12345-67890-09876-54321'));
+
+function auth (req, res, next) {
+
+  if (!req.signedCookies.user) {
+    var authHeader = req.headers.authorization;
+    if (!authHeader) {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+        return;
+    }
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+    var user = auth[0];
+    var pass = auth[1];
+    if (user == 'admin' && pass == 'password') {
+        res.cookie('user','admin',{signed: true});
+        next(); // authorized
+    } else {
+        var err = new Error('You are not authenticated!');
+        res.setHeader('WWW-Authenticate', 'Basic');              
+        err.status = 401;
+        next(err);
+    }
+  }
+  else {
+      if (req.signedCookies.user === 'admin') {
+          next();
+      }
+      else {
+          var err = new Error('You are not authenticated!');
+          err.status = 401;
+          next(err);
+      }
   }
 }
-
 app.use(auth);
+
 app.use(express.static(__dirname + '/public'));app.use('/dishes', dishRouter);
 app.use('/dishes/:dishId',dishRouter);
 app.use(express.static(__dirname + '/public'));app.use('/promotions', promotionRouter);
