6919b94f0d462a4cf5326ea1bc0b79525ca158e6
kvvsatyaravi <kvvsatyaravi318@gmail.com>
2020-08-21 14:39:03 +0530

    passsport

--- conFusionServer/app.js
+++ conFusionServer/app.js
@@ -7,21 +7,21 @@ var session = require('express-session');
 var FileStore = require('session-file-store')(session);
 const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/conFusion';
+var passport = require('passport');
+var authenticate = require('./authenticate');
+const dishRouter = require('./routes/dishrouter');
+const leaderRouter = require('./routes/leaderrouter');
+const promotionRouter = require('./routes/promorouter');
+const userRouter = require('./routes/users');
+const indexRouter= require('./routes/index');
+const bodyParser = require('body-parser');
+
 const connect = mongoose.connect(url);
 connect.then((db) => {
     console.log("Connected correctly to server");
 }, (err) => { 
   console.log(err); 
 });
-
-const dishRouter = require('./routes/dishrouter');
-const leaderRouter = require('./routes/leaderrouter');
-const promotionRouter = require('./routes/promorouter');
-const userRouter = require('./routes/users');
-const indexRouter= require('./routes/index');
-
-
-const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const morgan = require('morgan');
 app.use(morgan('dev'));
@@ -34,42 +34,33 @@ app.use(session({
   store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', userRouter);
 
-
 function auth (req, res, next) {
-    console.log(req.session);
+  console.log(req.user);
 
-  if(!req.session.user) {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
+  if (!req.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    next(err);
   }
   else {
-    if (req.session.user === 'authenticated') {
-      next();
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-      err.status = 403;
-      return next(err);
-    }
+        next();
   }
 }
 
 app.use(auth);
 
-
-
-app.use(express.static(__dirname + '/public'));
-
 app.use(express.static(__dirname + '/public'));
 app.use('/dishes', dishRouter);
 app.use('/dishes/:dishId',dishRouter);
-app.use(express.static(__dirname + '/public'));app.use('/promotions', promotionRouter);
+app.use('/promotions', promotionRouter);
 app.use('/promotions/:promoId',promotionRouter);
-  app.use(express.static(__dirname + '/public'));app.use('/leaders', leaderRouter);
+app.use('/leaders', leaderRouter);
 app.use('/leaders/:leadId',leaderRouter);
 
 
